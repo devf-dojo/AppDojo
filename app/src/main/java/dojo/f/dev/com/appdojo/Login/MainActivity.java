a19a1875ba76fc29bb45a33153c18c226b804117
LuisJovany <jiovany@live.com.mx>
2017-08-03 20:57:29 -0500

    login

--- app/src/main/java/dojo/f/dev/com/appdojo/Login/MainActivity.java
+++ app/src/main/java/dojo/f/dev/com/appdojo/Login/MainActivity.java
@@ -1,13 +1,16 @@
-package dojo.f.dev.com.appdojo;
+package dojo.f.dev.com.appdojo.Login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dojo.f.dev.com.appdojo.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
+
 }
