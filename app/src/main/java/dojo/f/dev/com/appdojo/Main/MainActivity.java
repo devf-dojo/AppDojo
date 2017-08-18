00d17c07e6366ba9d565617065454073c151c775
Ernesto Ramirez <NearTox@outlook.com>
2017-08-18 12:40:50 -0500

    add function to saveCv

--- app/src/main/java/dojo/f/dev/com/appdojo/Main/MainActivity.java
+++ app/src/main/java/dojo/f/dev/com/appdojo/Main/MainActivity.java
@@ -20,6 +20,10 @@ import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import dojo.f.dev.com.appdojo.Data.Api.client.AppDojoClient;
 import dojo.f.dev.com.appdojo.Data.Api.client.AppDojoService;
 import dojo.f.dev.com.appdojo.Data.Api.models.Cv;
@@ -29,6 +33,107 @@ import dojo.f.dev.com.appdojo.utils.LiveUser;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    AppDojoClient api = new AppDojoClient();
+
+    boolean has_mod = false;
+
+    TextView name;
+    TextView email;
+
+    SimpleDraweeView photo;
+
+    TextView phone;
+    TextView biography;
+    TextView skills;
+    TextView languages;
+    TextView website;
+    TextView github;
+    TextView facebook;
+    TextView twitter;
+    TextView linkedin;
+    TextView hobbies;
+
+    void saveCv() {
+        FirebaseUser user = LiveUser.mAuth.getCurrentUser();
+        if (user == null) {
+            return;
+        }
+        Cv cv = new Cv();
+        // TODO validate the info of user
+
+        cv.setName(name.getText().toString().trim());
+        String my_email = email.getText().toString().trim();
+        if (my_email.length() != 0) {
+            cv.setEmail(my_email);
+        }
+        /*if (photo. != null) {
+            cv.setPhoto()
+            photo.setImageURI();
+        }*/
+        cv.setPhone(phone.getText().toString().trim());
+
+        cv.setBiography(biography.getText().toString().trim());
+
+        String[] skills_array = skills.getText().toString().split(",");
+        List<String> skills_list = new ArrayList<String>();
+        for (int i = 0; i < skills_array.length; i++) {
+            String str = skills_array[i].trim();
+            if (str.length() != 0) {
+                skills_list.add(str);
+            }
+        }
+        if (skills_list.size() != 0) {
+            cv.setSkills(skills_list);
+        }
+
+        String[] languages_array = languages.getText().toString().split(",");
+        List<String> languages_list = new ArrayList<String>();
+        for (int i = 0; i < languages_array.length; i++) {
+            String str = languages_array[i].trim();
+            if (str.length() != 0) {
+                languages_list.add(str);
+            }
+        }
+        if (languages_list.size() != 0) {
+            cv.setLanguages(languages_list);
+        }
+
+        String my_website = website.getText().toString().trim();
+        if (my_website.length() != 0) {
+            cv.setWebsite(my_website);
+        }
+        String my_github = github.getText().toString().trim();
+        if (my_github.length() != 0) {
+            cv.setGithub(my_github);
+        }
+        String my_facebook = facebook.getText().toString().trim();
+        if (my_github.length() != 0) {
+            cv.setFacebook(my_facebook);
+        }
+        String my_twitter = twitter.getText().toString().trim();
+        if (my_twitter.length() != 0) {
+            cv.setTwitter(my_twitter);
+        }
+        String my_linkedin = linkedin.getText().toString().trim();
+        if (my_linkedin.length() != 0) {
+            cv.setLinkedin(my_linkedin);
+        }
+
+        String[] hobbies_array = hobbies.getText().toString().split(",");
+        List<String> hobbies_list = new ArrayList<String>();
+        for (int i = 0; i < hobbies_array.length; i++) {
+            String str = hobbies_array[i].trim();
+            if (str.length() != 0) {
+                hobbies_list.add(str);
+            }
+        }
+        if (hobbies_list.size() != 0) {
+            cv.setHobbies(hobbies_list);
+        }
+
+        api.updateCv(user.getUid(), cv);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +149,13 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //if (has_mod) {
+                saveCv();
+
+                Snackbar.make(view, "Saved", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                has_mod = false;
+                //}
             }
         });
 
@@ -59,9 +169,22 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
 
+        name = findViewById(R.id.name_cv);
+        email = findViewById(R.id.email_cv);
+        photo = findViewById(R.id.photo_cv);
+        phone = findViewById(R.id.phone_cv);
+        biography = findViewById(R.id.biography_cv);
+        skills = findViewById(R.id.skills_cv);
+        languages = findViewById(R.id.languages_cv);
+        website = findViewById(R.id.website_cv);
+        github = findViewById(R.id.github_cv);
+        facebook = findViewById(R.id.facebook_cv);
+        twitter = findViewById(R.id.twitter_cv);
+        linkedin = findViewById(R.id.linkedin_cv);
+        hobbies = findViewById(R.id.hobbies_cv);
+
         final FirebaseUser user = LiveUser.mAuth.getCurrentUser();
         if (user != null) {
-            final AppDojoClient api = new AppDojoClient();
             api.getCv(user.getUid(), new AppDojoService.onGetCvListener() {
                 @Override
                 public void onGetCvListener(@Nullable Cv cv, boolean is_success_full) {
@@ -69,45 +192,56 @@ public class MainActivity extends AppCompatActivity
                         return;
                     }
 
-                    TextView name = findViewById(R.id.name_cv);
-                    TextView email = findViewById(R.id.email_cv);
-                    SimpleDraweeView photo = findViewById(R.id.photo_cv);
-                    TextView phone = findViewById(R.id.phone_cv);
-                    TextView biography = findViewById(R.id.biography_cv);
-                    TextView website = findViewById(R.id.website_cv);
-                    TextView github = findViewById(R.id.github_cv);
-                    TextView facebook = findViewById(R.id.facebook_cv);
-                    TextView twitter = findViewById(R.id.twitter_cv);
-                    TextView linkedin = findViewById(R.id.linkedin_cv);
-
                     name.setText(cv.getName());
                     email.setText(cv.getEmail());
-                    photo.setImageURI(cv.getPhoto());
+                    if (cv.getPhoto() != null) {
+                        photo.setImageURI(cv.getPhoto());
+                    }
                     phone.setText(cv.getPhone());
+
                     biography.setText(cv.getBiography());
+
+                    String skills_list = "";
+                    if (cv.getSkills() != null) {
+                        for (String skill : cv.getSkills()) {
+                            skills_list += skill + ", ";
+                        }
+                    }
+                    skills.setText(skills_list);
+
+                    String language_list = "";
+                    if (cv.getLanguages() != null) {
+                        for (String language : cv.getLanguages()) {
+                            language_list += language + ", ";
+                        }
+                    }
+                    languages.setText(language_list);
+
                     website.setText(cv.getWebsite());
                     github.setText(cv.getGithub());
                     facebook.setText(cv.getFacebook());
                     twitter.setText(cv.getTwitter());
                     linkedin.setText(cv.getLinkedin());
+
+                    String hobbies_list = "";
+                    if (cv.getHobbies() != null) {
+                        for (String hobbies : cv.getHobbies()) {
+                            hobbies_list += hobbies + ", ";
+                        }
+                    }
+                    hobbies.setText(hobbies_list);
                 }
             });
         } else {
             return;
         }
         {
-            TextView name = findViewById(R.id.name_cv);
             name.setText(user.getDisplayName());
-
-            TextView email = findViewById(R.id.email_cv);
             email.setText(user.getEmail());
-
-            SimpleDraweeView photo = findViewById(R.id.photo_cv);
             photo.setImageURI(user.getPhotoUrl());
         }
         {
             View header = navigationView.getHeaderView(0);
-            ;
 
             TextView email = (TextView) header.findViewById(R.id.nav_email);
             email.setText(user.getEmail());
